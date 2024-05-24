21ce77724524f46375b4433eb996249d112d0bd5
gonzalorportfolio <gromeroeducation@gmail.com>
2024-05-24 18:23:51 -0400

    cross my fingers

--- server/knexfile.js
+++ server/knexfile.js
@@ -7,7 +7,7 @@ const seedsDirectory = path.join(__dirname, '/db/seeds');
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
+    connection: process.env.PG_CONNECTION_STRING || {
       host: process.env.PG_HOST || '127.0.0.1',
       port: process.env.PG_PORT || 5432,
       user: process.env.PG_USER || 'postgres',
